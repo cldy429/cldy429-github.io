59db87051970777ff1a6af35a74164b46b7633c9
cldy429 <g.temuulen429@gmail.com>
2024-12-12 07:36:11 +0800

    gameover hesgiig nemev

---
+++ lab/lab9/script.js
@@ -0,0 +1,7 @@
+// Асуулт хариулт
+const questions = [
+    { question: "A fruit that keeps the doctor away", answer: "apple" },
+    { question: "A yellow curved fruit", answer: "banana" },
+    { question: "A red fruit often associated with love", answer: "cherry" },
+    { question: "A green or purple fruit in clusters", answer: "grape" }
+];
